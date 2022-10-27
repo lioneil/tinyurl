120ebf4ce5528881d97aa50b4acbd62944440c56
John Lioneil Dionisio <john.dionisio1@gmail.com>
2022-10-28 06:25:15 +0800

    add: Scope Search class for search-related queries

--- app/Models/Destination.php
+++ app/Models/Destination.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
+use App\Enums\DestinationStatus;
+use App\Models\Traits\HasSearch;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Destination extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSearch;
+
+    /**
+     * Searchable columns.
+     *
+     * @var array
+     */
+    public $searchableColumns = ['url', 'alias'];
+
+
+    /**
+     * Scope a query to search through columns.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    function scopeActiveOnly (Builder $query)
+    {
+        return $query->where('status', DestinationStatus::ACTIVE);
+    }
 }
---
+++ app/Models/Traits/HasSearch.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models\Traits;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Schema;
+
+trait HasSearch
+{
+    /**
+     * Unsearchable columns.
+     *
+     * @var array
+     */
+    public $unsearchableColumns = ['id', 'updated_at', 'created_at'];
+
+    /**
+     * Cleanup columns and remove unsearchable fields.
+     *
+     * @param array $columns
+     * @return array
+     */
+    function sanitizeSearchableColumns (array $columns)
+    {
+        return collect($columns)->reject(function ($v, $column) {
+            return in_array($column, $this->unsearchableColumns);
+        })->toArray();
+    }
+
+    /**
+     * Scope a query to search through columns.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $search
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    function scopeSearch (Builder $query, $search)
+    {
+        if (empty($search)) {
+            return $query;
+        }
+
+        return $query->where(function ($builder) use ($search) {
+            foreach ($this->searchableColumns as $column) {
+                $builder->orWhere($column, 'like', "%{$search}%");
+            }
+        });
+    }
+}
