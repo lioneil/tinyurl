98421e0126d33cce77dd8291d7e0db96606b68c5
John Lioneil Dionisio <john.dionisio1@gmail.com>
2022-10-28 15:28:37 +0800

    add: tags and destination_tag migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Destination;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,7 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $tags = Tag::factory(100)->create();
         $destinations = Destination::factory(500)->create();
+        $destinations->each(function ($destination) use ($tags) {
+            $tags->random(rand(1, 10))->each(fn($tag) => $destination->tags()->attach($tag->id));
+        });
         $destinations->random(100)->each(fn($destination) => $destination->delete());
     }
 }
