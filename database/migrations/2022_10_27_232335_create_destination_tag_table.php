14812074b0f2082f329d7b6695d214fad95167cd
John Lioneil Dionisio <john.dionisio1@gmail.com>
2022-10-28 15:28:02 +0800

    add: tags and destintion_tag migration

---
+++ database/migrations/2022_10_27_232335_create_destination_tag_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('destination_tag', function (Blueprint $table) {
+            $table->bigInteger('destination_id')->unsigned()->index();
+            $table->bigInteger('tag_id')->unsigned()->index();
+            $table->foreign('destination_id')->references('id')->on('destinations')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('destination_tag');
+    }
+};
