c17c71c2ac194551463724b2eb6cbb3a2d4295f1
John Lioneil Dionisio <john.dionisio1@gmail.com>
2022-10-28 15:30:04 +0800

    refactor: cleanup code and add orSearch alternative scope

--- app/Models/Traits/HasSearch.php
+++ app/Models/Traits/HasSearch.php
@@ -3,7 +3,6 @@
 namespace App\Models\Traits;
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\Schema;
 
 trait HasSearch
 {
@@ -32,26 +31,78 @@ trait HasSearch
      *
      * @param  \Illuminate\Database\Eloquent\Builder  $query
      * @param  string  $search
+     * @param  string  $relatedTable
+     * @param  array  $relatedTableColumns
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    function scopeSearch (Builder $query, $search)
+    function scopeSearch (Builder $query, $search, $relatedTable = null, $relatedTableColumns = [])
     {
         if (empty($search)) {
             return $query;
         }
 
-        return $query->where(function ($builder) use ($search) {
-            if (str_contains($search, "\"")) {
-                // Case-sensitive search
-                foreach ($this->searchableColumns as $column) {
-                    $builder->orWhere($column, trim($search, '"'));
-                }
-            } else {
-                // Case-insensitive search
-                foreach ($this->searchableColumns as $column) {
+        if (empty($relatedTable)) {
+            return $query->where(function ($builder) use ($search) {
+                $this->performSearch($builder, $search);
+            });
+        }
+
+        return $query->whereHas($relatedTable, function ($builder) use ($search, $relatedTableColumns) {
+            $this->performSearch($builder, $search, $relatedTableColumns, $strict = true);
+        });
+    }
+
+    /**
+     * Scope a query to search through columns.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $search
+     * @param  string  $relatedTable
+     * @param  array  $relatedTableColumns
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    function scopeOrSearch (Builder $query, $search, $relatedTable = null, $relatedTableColumns = [])
+    {
+        if (empty($search)) {
+            return $query;
+        }
+
+        if (empty($relatedTable)) {
+            return $query->orWhere(function ($builder) use ($search) {
+                $this->performSearch($builder, $search);
+            });
+        }
+
+        return $query->orWhereHas($relatedTable, function ($builder) use ($search, $relatedTableColumns) {
+            $this->performSearch($builder, $search, $relatedTableColumns, $strict = true);
+        });
+    }
+
+    /**
+     * Perform case-sensitive or insensitive search.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $builder
+     * @param string $search
+     * @param array $columns
+     * @return void
+     */
+    function performSearch (Builder $builder, $search, $columns = [], $strict = false)
+    {
+        $columns = empty($columns) ? $this->searchableColumns : $columns;
+        if (str_contains($search, "\"")) {
+            // Case-sensitive search
+            foreach ($columns as $column) {
+                $builder->orWhere($column, trim($search, '"'));
+            }
+        } else {
+            // Case-insensitive search
+            foreach ($columns as $column) {
+                if ($strict) {
+                    $builder->where($column, 'like', "%{$search}%");
+                } else {
                     $builder->orWhere($column, 'like', "%{$search}%");
                 }
             }
-        });
+        }
     }
 }
